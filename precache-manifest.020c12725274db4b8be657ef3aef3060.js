954632db4da0456a38f72bad1c93762070836271
ashishghosh123 <smart.ash43@yahoo.com>
2020-11-23 10:59:41 +0530

    Updates

---
+++ precache-manifest.020c12725274db4b8be657ef3aef3060.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2b740913c7e26c2ca33809a4f471c89",
+    "url": "/Netflix-clone/index.html"
+  },
+  {
+    "revision": "584da5e1f57439637390",
+    "url": "/Netflix-clone/static/css/main.4e645c5d.chunk.css"
+  },
+  {
+    "revision": "7a303857a26df437859d",
+    "url": "/Netflix-clone/static/js/2.30d140c3.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/Netflix-clone/static/js/2.30d140c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "584da5e1f57439637390",
+    "url": "/Netflix-clone/static/js/main.feb2a67b.chunk.js"
+  },
+  {
+    "revision": "2e6a87f45ab8bf4ca6d8",
+    "url": "/Netflix-clone/static/js/runtime-main.640d35e1.js"
+  }
+]);
